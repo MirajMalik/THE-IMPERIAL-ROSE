e9eb01ced08c53789c203072ea5c3ada9ff371c9
MirajMalik <mirajmalik333@gmail.com>
2025-08-06 01:49:26 +0600

    photo gallery added

--- frontend/desgin/js/script.js
+++ frontend/desgin/js/script.js
@@ -8,3 +8,28 @@ window.onscroll = () =>{
    navbar.classList.remove('active');
 }
 
+var swiper = new Swiper(".home-slider", {
+   loop:true,
+   effect: "coverflow",
+   spaceBetween: 30,
+   grabCursor: true,
+   autoplay: {
+   delay: 3000, 
+   disableOnInteraction: false,
+   },
+   coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
+   },
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+});
